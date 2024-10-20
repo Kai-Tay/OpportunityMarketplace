97d287ee4aab9f68c08d342d03720a9427d400dd
Kai-Tay <kaishengtay82@gmail.com>
2024-10-20 21:39:30 +0800

    Added Heading for Job Content

--- OpportunityMarketplace/src/app/content/content.component.ts
+++ OpportunityMarketplace/src/app/content/content.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { LearnMoreComponent } from "./learn-more/learn-more.component";
 import { FilterOptionsComponent } from "./filter-options/filter-options.component";
+import { JobCardComponent } from "./job-card/job-card.component";
+import { CircularIconComponent } from '../circular-icon/circular-icon.component';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [LearnMoreComponent, FilterOptionsComponent],
+  imports: [LearnMoreComponent, FilterOptionsComponent, JobCardComponent, CircularIconComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
