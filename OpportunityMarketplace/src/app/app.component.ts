dc9c4ded24f70a5106027e5e0b2978eadbab22b1
Kai-Tay <kaishengtay82@gmail.com>
2024-10-20 13:55:06 +0800

    Added filter-bar

--- OpportunityMarketplace/src/app/app.component.ts
+++ OpportunityMarketplace/src/app/app.component.ts
@@ -2,21 +2,19 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PrimeNGConfig } from 'primeng/api';
-import { Aura } from 'primeng/themes/aura';
+
 import { ContentComponent } from "./content/content.component";
+import { FilterBarComponent } from "./filter-bar/filter-bar.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ContentComponent], 
+  imports: [RouterOutlet, NavbarComponent, ContentComponent, FilterBarComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   title = 'OpportunityMarketplace';
-  constructor(private config: PrimeNGConfig) {
-    this.config.theme.set({ preset: Aura });
-}
 }
